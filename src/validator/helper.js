d88c5cbdd1cd5c7a0870c6fd828ac7223802303f
prakash-urkude <prakashurkude42416@gmail.com>
2022-10-15 14:12:07 +0530

    add file

---
+++ src/validator/helper.js
@@ -0,0 +1,9 @@
+const str = "functonUP"
+
+const tr = function(){return str.trim()}
+const upr = function(){return str.trim()}
+const lwr = function(){return str.toLowerCase()}
+
+module.exports.tr =tr
+module.exports.upr =upr
+module.exports.lwr =lwr
