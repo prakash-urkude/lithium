dc0a4736488e6aea261e79a0829495310a2ece79
prakash-urkude <prakashurkude42416@gmail.com>
2022-10-15 10:25:43 +0530

    adding file

---
+++ src/logger/logger.js
@@ -0,0 +1,9 @@
+let welcome= function(){
+    console.log ("Welcome to my application. I am Prakash Urkude and a part of FunctionUp Lithium cohort.")
+    return "done"
+}                                          //so now when we call it'll give me this line.
+
+module.exports.welcome1= welcome           //  this functions are by default private,so we are making this function public by giving this a new name.
+
+
+                                            //now we have to go inside our folder git bash in terminal. and run the server, so that when we call it'll give the responce.
