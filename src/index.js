112f444e153c68450310edaabd9a5939c06a33ab
AbhijitRadke <radkeabhi27@gmail.com>
2022-11-28 14:45:51 +0530

    index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const route = require('./routes/route.js');
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb+srv://prakashurkude:prakash1998@cluster0.nuhssqs.mongodb.net/project/booksManagementGroup4",
+    { useNewUrlParser: true })
+    .then(() => console.log('mongodb is connected'))
+    .catch(err => console.log(err))
+
+app.use('/', route);
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
--- src/validators/validator.js
+++ src/validators/validator.js
@@ -2,7 +2,7 @@ const isValidRequestBody = function (requestBody) {
     return Object.keys(requestBody).length > 0;
 }
 const isValid = function (value) {
-    if (typeof value === 'undefined' ) return false;
+    if (typeof value === 'undefined') return false;
     if (typeof value === 'string' && value.trim().length === 0) return false
     return true;
 }
@@ -15,15 +15,28 @@ const isValidName = function (abc) {
 }
 
 const checkPassword = function (str) {
-    var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,15}$/;
+    var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{7,15}$/;
     return re.test(str);
 }
 
+const isValidNumber = function (num) {
+    const reg = /^[0-9]{10}$/;
+    return reg.test(String(num));
+}
 
+const isValidPincode = function (num) {
+    const reg = /^[0-9]{6}$/;
+    return reg.test(String(num));
+}
+
+const isValidISBN = function (num) {
+    const reg = /^[0-9]{10,13}$/;
+    return reg.test(String(num));
+}
 
 function isValidEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 
-module.exports = { isValid, isValidEmail, isValidRequestBody, isValidName, checkPassword }
+module.exports = { isValid, isValidEmail, isValidRequestBody, isValidName, isValidNumber, isValidPincode, isValidISBN, checkPassword }
