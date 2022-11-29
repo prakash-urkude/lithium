8159a4540b3e1e2e9f55020017947021f1e92e79
AbhijitRadke <radkeabhi27@gmail.com>
2022-11-29 12:18:35 +0530

    read me

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -10,7 +10,7 @@ const createBook = async function (req, res) {
     try {
         const data = req.body
         if (Object.keys(data) == 0) return res.status(400).send({ status: false, message: "No input provided" });
-         const { title, excerpt, userId, ISBN, category, subcategory, reviews, releasedAt } = data
+        const { title, excerpt, userId, ISBN, category, subcategory, reviews, releasedAt } = data
 
         if (!userId) return res.status(400).send({ status: false, message: "Please enter userId" })
         if (!isValidObjectId(userId)) return res.status(404).send({ status: false, message: "user not found for this user Id" })
@@ -20,9 +20,9 @@ const createBook = async function (req, res) {
         if (!title) return res.status(400).send({ status: false, message: "Please Enter Title" })
         if (!Validation.isValid(title)) return res.status(400).send({ status: false, message: "please inter valid title" })
         let findTitle = await bookModel.find({ title: title })
-        if (findTitle) return res.status(400).send({ status: false, message: "Book allrady exist for this tile " })
+        if (findTitle) return res.status(400).send({ status: false, message: "Book allrady exist for this title " })
 
-        
+
         //  -------------------------------SIBN Validation-----------------------
         if (!ISBN) return res.status(400).send({ status: false, message: "Please enter ISBN" })
         if (!Validation.isValidISBN(ISBN)) return res.status(400).send({ status: false, message: "please inter valid ISBN" })
@@ -41,8 +41,8 @@ const createBook = async function (req, res) {
         if (!subcategory) return res.status(400).send({ status: false, message: "Please enter subcategory" })
         if (!Validation.isValid(subcategory)) return res.status(400).send({ status: false, message: "please inter valid subcategory" })
 
-        
-        data.releasedAt = new Date()
+
+        data.releasedAt = new Date(date.now())
         const bookData = await bookModel.create(data)
         res.status(201).send({ status: true, data: bookData })
     }
